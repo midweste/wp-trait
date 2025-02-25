95b768c788923f0130db2d3294f0ccadb1d34b6d
midwestE <midwestE@devbunker.com>
2025-02-25 11:20:25 -0600

    Cast to array

--- src/Collection/Route.php
+++ src/Collection/Route.php
@@ -31,7 +31,7 @@ if (!class_exists('WPTrait\Collection\Route')) {
             }
 
             # Sanitize method name
-            $args['methods'] = array_map('strtoupper', $args['methods']);
+            $args['methods'] = array_map('strtoupper', (array) $args['methods']);
 
             # Sanitize Callback Method
             if (is_string($args['callback'])) {
